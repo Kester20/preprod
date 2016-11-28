636aa6c5dfeb5ea1543d866759aa3c671f87cfc1
Said Sulaiman Arsalan Nurmal <said_sulaiman_arsalan_nurmal@epam.com>
2016-11-28 17:53:30 +0200

    in progress task10

--- KharkivJavaTask10/src/main/webapp/js/validation.js
+++ KharkivJavaTask10/src/main/webapp/js/validation.js
@@ -1,36 +1,75 @@
 
 function validateRegisterForm (){
-    //alert(document.getElementsByClassName("text-info")[0].lastChild.textContent + "invalid input");
-	return isNotEmptyInput() && checkLengthPassword(document.registerForm.pass.value)
-	 && validateEmail(document.registerForm.email.value)
-	 && validatePhone(document.registerForm.mobileNumber.value);
+    var valid = true;
+    var inputs = document.registerForm.getElementsByTagName("input");
+        for (i=0; i<inputs.length - 1; i++){
+            if(inputs[i].className == "invalid" || inputs[i].value == ""){
+                valid = false;
+            }
+        }
+    return valid;
 }
 
-function isNotEmptyInput(){
-    isNotEmpty = true;
-    var inputs = document.registerForm.getElementsByTagName("input");
-    for (i=0; i<inputs.length; i++){
-        if(inputs[i].value == ""){
-            isNotEmpty = false;
-            inputs[i].className = "error";
-            var s = document.getElementsByClassName("text-info")[i].lastChild.textContent;
-            document.getElementsByClassName("text-info")[i].lastChild.textContent = s + "invalid input";
-            //break;
-        }
-    }
-    return isNotEmpty;
+function setInvalidClass(element){
+    element.parentNode.previousSibling.previousSibling.className = "text-info-invalid";
+    element.className = "invalid";
+}
+
+function setValidClass(element){
+    element.parentNode.previousSibling.previousSibling.className = "text-info";
+    element.className = "valid";
+}
+
+function isNotEmptyInput(input){
+    return input.value != "";
 }
 
 function checkLengthPassword(pass){
-    return pass.length >= 8;
+    var result = pass.value.length >= 8;
+    if(!result){
+        setInvalidClass(pass);
+        pass.parentNode.nextSibling.nextSibling.innerHTML = "Password less then 8 symbols";
+    }else{
+        setValidClass(pass);
+        pass.parentNode.nextSibling.nextSibling.innerHTML = "";
+    }
+    return result;
 }
 
 function validateEmail(email) {
     var regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-    return regex.test(email);
+    var result = regex.test(email.value);
+    if(!result){
+        setInvalidClass(email);
+        email.parentNode.nextSibling.nextSibling.innerHTML = "Invalidate email format";
+    }else{
+        setValidClass(email);
+        email.parentNode.nextSibling.nextSibling.innerHTML = "";
+    }
+    return result;
 }
 
 function validatePhone(phone){
     var regex = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4,5})$/;
-    return regex.test(phone);
+    var result = regex.test(phone.value);
+    if(!result){
+        setInvalidClass(phone);
+        phone.parentNode.nextSibling.nextSibling.innerHTML = "Invalidate mobile number format";
+    }else{
+        setValidClass(phone);
+        phone.parentNode.nextSibling.nextSibling.innerHTML = "";
+    }
+    return result;
+}
+
+function checkStringEquals(element, element2){
+    var result = element.value == element2.value;
+    if(!result){
+        setInvalidClass(element2);
+        element2.parentNode.nextSibling.nextSibling.innerHTML = "Passwords are different";
+    }else{
+        setValidClass(element2);
+        element2.parentNode.nextSibling.nextSibling.innerHTML = "";
+    }
+    return result;
 }
